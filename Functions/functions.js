3bba392b84589edfa43f3aa4bea422e95d45d278
Ben <b4willys@gmail.com>
2024-03-11 15:29:55 -0500

    function stuff

---
+++ Functions/functions.js
@@ -0,0 +1,14 @@
+function ourReusableFunction(){
+    console.log('Hello World!');
+}
+
+ourReusableFunction();
+
+
+function doSomeMath(math1, math2){
+    var sum = 0;
+    sum = math1 + math2;
+    console.log(sum);
+}
+
+doSomeMath(6,7);
